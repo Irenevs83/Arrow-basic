637f4d6353dadaf647a6c5bc15e37abaa723b9b0
Irenevs83 <80913299+Irenevs83@users.noreply.github.com>
2021-05-12 14:30:59 +0200

    Add files via upload

---
+++ Arrow_Function/Arrows.js
@@ -0,0 +1,25 @@
+function nrs([N1, N2]) {
+    const sm = ((N1 * N1) + (N2 * N2));
+    return sm * sm;
+};
+const rslt = nrs([2, 2]);
+console.log(rslt);
+
+
+
+
+const numbers = function ([num1, num2]) {
+    const som = ((num1 * num1) + (num2 * num2));
+    return som * som;
+};
+const result = numbers([5, 5]);
+console.log(result);
+
+
+
+const numbs = ([numb1, numb2]) => {
+    const Som = ((numb1 * numb1) + (numb2 * numb2));
+    return Som * Som;
+};
+const results = numbs([10, 10]);
+console.log(results);
